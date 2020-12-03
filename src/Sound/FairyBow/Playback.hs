54ada46614d36e7a1f452319b76cbd1187539f38
Alex Stuart <alex@das.li>
2020-12-03 13:47:27 -0800

    Used UnitReal type for mixer fading

--- src/Sound/FairyBow/Playback.hs
+++ src/Sound/FairyBow/Playback.hs
@@ -5,7 +5,7 @@ module Sound.FairyBow.Playback where
 import              Data.FairyBow.Audio
 import              FairyBowPlatformType
 import              SDL.Mixer hiding (Audio, Channel)
-import qualified    SDL.Mixer (Audio, Channel)
+import qualified    SDL.Mixer (Channel)
 import              Sound.Lightarrow.Playback
 import              System.FairyBow.Actuation
 import              System.Lightarrow.Actuation
@@ -13,10 +13,10 @@ import              System.Lightarrow.Actuation
 instance ActuatePlatform (FairyBow os)
             => MixerPlatform (FairyBow os) where
     data Channel (FairyBow os) = Channel SDL.Mixer.Channel
-    channel x                       = Channel (fromIntegral x :: SDL.Mixer.Channel)
-    stop (Channel c)                = Actuation [DirectOut (halt c)]
-    fade (Channel c) (UnitReal x)   = let v = floor (x * 128)
-                                        in Actuation [DirectOut (setVolume v c)]
+    channel x           = Channel (fromIntegral x :: SDL.Mixer.Channel)
+    stop (Channel c)    = Actuation [DirectOut (halt c)]
+    fade (Channel c) x  = let v = floor (fromUnitReal x * 128)
+                            in Actuation [DirectOut (setVolume v c)]
 
 instance (ActuatePlatform (FairyBow os), MixerPlatform (FairyBow os))
             => PlaybackPlatform (FairyBow os) where
@@ -25,8 +25,8 @@ instance (ActuatePlatform (FairyBow os), MixerPlatform (FairyBow os))
                         Just chunk  -> [    DirectOut (do   playOn chan Once chunk
                                                             return ())  ]
                         Nothing     -> [])
-    onLoop (Channel c) (Audio _ mc) 
+    onLoop (Channel chan) (Audio _ mc) 
         = Actuation (case mc of
-                        Just chunk  -> [    DirectOut (do   playOn c Forever chunk
+                        Just chunk  -> [    DirectOut (do   playOn chan Forever chunk
                                                             return ())  ]
                         Nothing     -> [])
